e94582e949d4baa9da464fa2ff0503bf1fe3d02f
kyle <kdeak1210@gmail.com>
2017-11-07 14:56:30 -0500

    Setting up some Express

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const index = require('./routes/index')
+const api = require('./routes/api')
+
+const app = express()
+
+// Connect to MongoDB via mongoose
+mongoose.connect(process.env.MONGO_URL, {useMongoClient: true}, (err) => {
+  if (err){
+    console.log('Failure to connect to MongoDB')
+    return
+  }
+  
+  console.log('Successfully connected to MongoDB')
+})
+
+// Apply some middleware
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Handlebars Templating Engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// Routes
+app.use('/', index)
+app.use('/api', api)
+
+// Spin up the server
+const port = process.env.port || 3000
+app.listen(port, (err) => {
+  if (err){
+    console.log(`Error while trying to connect to port ${port}`)
+    return
+  }
+
+  console.log(`Server started on port ${port}`)
+})
